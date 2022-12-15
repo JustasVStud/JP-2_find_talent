2027eb9e680e867739cf6ea8cb28fd79b4cfb0b3
Egidijus Jakavonis <e.jakavonis@gmail.com>
2022-12-15 23:10:56 +0200

    padaryta mobili versija uzduoties JP2FT-11

---
+++ about-script.js
@@ -0,0 +1,83 @@
+//Nikita
+const bindNikita = document.getElementById("bind-plus-nikita");
+const nikitaPosition =  document.getElementById('nikita-position');
+const nikitaPhoto = document.getElementById('nikita-photo');
+const nikitaDescription = document.getElementById('nikita-description');
+
+
+bindNikita.addEventListener('click', function(){
+    bindNikita.classList.toggle("active");
+    nikitaPosition.classList.toggle('show');
+    nikitaDescription.classList.toggle('show');
+    nikitaPhoto.classList.toggle('show');
+});
+
+//Cristian
+const bindCristian = document.getElementById("bind-plus-cristian");
+const CristianPosition =  document.getElementById('cristian-position');
+const CristianPhoto = document.getElementById('cristian-photo');
+const CristianDescription = document.getElementById('cristian-description');
+
+
+bindCristian.addEventListener('click', function(){
+    bindCristian.classList.toggle("active");
+    CristianPosition.classList.toggle('show');
+    CristianDescription.classList.toggle('show');
+    CristianPhoto.classList.toggle('show');
+});
+
+//Cruz
+const bindCruz = document.getElementById("bind-plus-cruz");
+const CruzPosition =  document.getElementById('cruz-position');
+const CruzPhoto = document.getElementById('cruz-photo');
+const CruzDescription = document.getElementById('cruz-description');
+
+
+bindCruz.addEventListener('click', function(){
+    bindCruz.classList.toggle("active");
+    CruzPosition.classList.toggle('show');
+    CruzDescription.classList.toggle('show');
+    CruzPhoto.classList.toggle('show');
+});
+
+//Drake
+const bindDrake = document.getElementById("bind-plus-drake");
+const DrakePosition =  document.getElementById('drake-position');
+const DrakePhoto = document.getElementById('drake-photo');
+const DrakeDescription = document.getElementById('drake-description');
+
+
+bindDrake.addEventListener('click', function(){
+    bindDrake.classList.toggle("active");
+    DrakePosition.classList.toggle('show');
+    DrakeDescription.classList.toggle('show');
+    DrakePhoto.classList.toggle('show');
+});
+
+//Griffin
+const bindGriffin = document.getElementById("bind-plus-griffin");
+const GriffinPosition =  document.getElementById('griffin-position');
+const GriffinPhoto = document.getElementById('griffin-photo');
+const GriffinDescription = document.getElementById('griffin-description');
+
+
+bindGriffin.addEventListener('click', function(){
+    bindGriffin.classList.toggle("active");
+    GriffinPosition.classList.toggle('show');
+    GriffinDescription.classList.toggle('show');
+    GriffinPhoto.classList.toggle('show');
+});
+
+//Aden
+const bindAden = document.getElementById("bind-plus-aden");
+const AdenPosition =  document.getElementById('aden-position');
+const AdenPhoto = document.getElementById('aden-photo');
+const AdenDescription = document.getElementById('aden-description');
+
+
+bindAden.addEventListener('click', function(){
+    bindAden.classList.toggle("active");
+    AdenPosition.classList.toggle('show');
+    AdenDescription.classList.toggle('show');
+    AdenPhoto.classList.toggle('show');
+});
